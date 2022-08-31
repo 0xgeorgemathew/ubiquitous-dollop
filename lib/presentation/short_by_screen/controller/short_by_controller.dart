38ec3d6a3a2cb03ae5109a891eec265824915573
0xgeorgemathew <georgekurianmathew@gmail.com>
2022-08-31 18:37:56 +0000

    Code Synced by DhiWise

---
+++ lib/presentation/short_by_screen/controller/short_by_controller.dart
@@ -0,0 +1,16 @@
+import '/core/app_export.dart';
+import 'package:application1/presentation/short_by_screen/models/short_by_model.dart';
+
+class ShortByController extends GetxController {
+  Rx<ShortByModel> shortByModelObj = ShortByModel().obs;
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
